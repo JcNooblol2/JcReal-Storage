200dd1fa87fa00d03ace2a4514aadb1a993c721b
JcNooblol2 <important@jcmainclr.xyz>
2024-12-28 16:42:36 +0800

    Add files via upload

--- kupal.js
+++ kupal.js
@@ -1,9 +1,10 @@
+
 const xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.jcmainclr.lol/api/v1/christmas/countdown");
+xhr.open("GET", "https://api.jcmainclr.lol/api/v1/notif");
 xhr.onload = function() {
   if (xhr.status === 200) {
-    const shit = JSON.parse(xhr.responseText).days;
-     document.getElementById('hei').innerHTML = `${shit} days before christmas 🎄 (cool) `
+    const shit = JSON.parse(xhr.responseText);
+     document.getElementById('notif').innerHTML = `NOTIF: ${shit[0].content}`
   }
 }
 xhr.send();
